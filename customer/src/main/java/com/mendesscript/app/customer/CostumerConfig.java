f323e3c7cadd7a9b300206130eec9aff5bdc8200
Gustavo <srto.mendes@gmail.com>
2025-03-05 20:29:03 -0300

    rest template

---
+++ customer/src/main/java/com/mendesscript/app/customer/CostumerConfig.java
@@ -0,0 +1,15 @@
+package com.mendesscript.app.customer;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class CostumerConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
